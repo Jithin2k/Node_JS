d02d4e216d3f95db6d94ea041585f617b184a7a0
Jithin2k <jithinjmadhav@gmail.com>
2025-07-02 19:56:50 +0530

    Routing and HTTP METHODS

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use((req ,res) => {
-    res.send("Hello Welcome")
-})
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Jithin",
+    lastName: "Madhav",
+  });
+});
 
-app.listen(3000,() =>{
-    console.log("Server is running in http://localhost:3000 ")
+app.post("/user", (req, res) => {
+  console.log("Data Added to Database");
+  res.send("Data Successfully Added");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data Deleted");
+});
+
+// use will match all http routes
+app.use("/a", (req, res) => {
+  res.send("Hello Welcome To A");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running in http://localhost:3000 ");
 });

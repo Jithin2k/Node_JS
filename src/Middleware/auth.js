2852bcc0ea24bbfd407ec6f7f95e3b79f20604f9
Jithin2k <jithinjmadhav@gmail.com>
2025-07-03 20:45:02 +0530

    Error Handling and Middleware

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,23 +1,22 @@
- const adminAuth = (req,res,next) =>{
-    console.log("Admin auth is getting checked");
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if (!isAdminAuthorized) {
-        res.status(401).send("Unauthorized Request")
-    } else{
-        next();
-    }
-}
+const adminAuth = (req, res, next) => {
+  console.log("Admin auth is getting checked");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized Request");
+  } else {
+    next();
+  }
+};
 
-
- const userAuth = (req,res,next) =>{
-    console.log("Admin auth is getting checked");
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if (!isAdminAuthorized) {
-        res.status(401).send("Unauthorized Request")
-    } else{
-        next();
-    }
-}
-module.exports ={adminAuth,userAuth} ;
+const userAuth = (req, res, next) => {
+  console.log("User auth is getting checked");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized Request");
+  } else {
+    next();
+  }
+};
+module.exports = { adminAuth, userAuth };

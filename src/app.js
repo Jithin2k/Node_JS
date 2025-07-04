d370a2428f97b383ac1121f2cc4cd95c62baa45f
Jithin2k <jithinjmadhav@gmail.com>
2025-07-04 21:05:18 +0530

    Get API and find methods of modal

--- src/app.js
+++ src/app.js
@@ -7,16 +7,40 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("User Added to DB");
   } catch (error) {
     res.status(400).send("Failed to save User data");
   }
- });
+});
+
+// GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const users = await User.findOne({ email: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(404).send("Failed request");
+  }
+});
+
+// GET request to shoe all users
+
+app.get("/feed",async (req,res)=>{
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+     res.status(404).send("Failed request");
+  }
+})
 
 connectDB()
   .then(() => {
@@ -27,4 +51,4 @@ connectDB()
   })
   .catch((err) => {
     console.log("DB connecction Failed");
-  })
+  });

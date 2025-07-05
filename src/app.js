756520ec51d45a2b9285f2e7fd0d2ac7017529a0
Jithin2k <jithinjmadhav@gmail.com>
2025-07-05 21:22:45 +0530

    Schema Validation Methods

--- src/app.js
+++ src/app.js
@@ -61,7 +61,10 @@ app.patch("/user",async(req,res) =>{
   const userId = req.body.userId;
   const data = req.body;
   try {
-    await User.findByIdAndUpdate({_id : userId},data);
+   const user = await User.findByIdAndUpdate({_id : userId},data,{
+    returnDocument:"after",
+    runValidators:true,
+   });
     res.send("User Updated Successfully")
   } catch (error) {
      res.status(404).send("Failed request");
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,57 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    minLength: 4,
+    maxLength : 50,
+    lowercase: true
   },
   lastName: {
     type: String,
+    minLength: 4,
+    maxLength : 50,
+    lowercase: true
   },
   age: {
     type: Number,
+    min: 18,
   },
   email: {
     type: String,
+    lowercase: true,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    lowercase: true,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender data is not valid");
+      }
+    },
   },
   password: {
     type: String,
+    required: true,
+    trim: true,
+    minLength : 8
   },
-});
+  about: {
+    type: String,
+    default: "Available",
+    trim: true,
+  },
+  photoUrl: {
+    type: String,
 
-const User = mongoose.model("User",userSchema)
+  },
+  skills: {
+    type: [String],
+  },
+},{timestamps:true});
 
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;

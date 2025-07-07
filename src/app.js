e0d331cbd37a98948335f4fdb65b1ca8df69694a
Jithin2k <jithinjmadhav@gmail.com>
2025-07-07 22:13:39 +0530

    Encrypting passwors using bcrypt and user login implementation

--- src/app.js
+++ src/app.js
@@ -1,21 +1,65 @@
 const express = require("express");
 const connectDB = require("./Config/Database");
 const app = express();
+const bcrypt = require("bcrypt");
+const { validateSignUpData } = require("./Utils/validation");
 
 const User = require("./models/user");
 
 app.use(express.json());
 
+// SIGN UP API
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
   try {
-    if(user.skills.length > 10){
-      throw new Error("SKills cannot exceed 10")
+    // 1.Validation Of Data
+    validateSignUpData(req);
+
+    // 2.Encrypt The Password
+
+    const { password, firstName, lastName, email } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    console.log(passwordHash);
+
+    // User
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+
+    if (user.skills.length > 10) {
+      throw new Error("SKills cannot exceed 10");
     }
     await user.save();
+
     res.send("User Added to DB");
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).send("ERROR IS : " + error.message);
+  }
+});
+
+// LOGIN API
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("Email is not in Database");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      res.send("Login Successful");
+    } else {
+      throw new Error("Password is not correct");
+    }
+  } catch (error) {
+    res.status(400).send("ERROR IS : " + error.message);
   }
 });
 
@@ -34,61 +78,64 @@ app.get("/user", async (req, res) => {
   }
 });
 
-
 // GET request to show all users
 
-app.get("/feed",async (req,res)=>{
+app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
     res.send(users);
   } catch (error) {
-     res.status(404).send("Failed request");
+    res.status(404).send("Failed request");
   }
-})
+});
 
 // DELETE a user by ID
-app.delete("/user",async(req,res) => {
+app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
-  console.log(userId)
+  console.log(userId);
   try {
     const user = await User.findByIdAndDelete(userId);
-    res.send("User Deleted Successfully")
+    res.send("User Deleted Successfully");
   } catch (error) {
     res.status(404).send(error.message);
   }
-})
+});
 
 // Update DAta of user
 
-app.patch("/user/:userId",async(req,res) =>{
+app.patch("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
   const data = req.body;
   try {
+    const ALLOWED_UPDATES = [
+      "age",
+      "skills",
+      "photoUrl",
+      "about",
+      "gender",
+      "userId",
+    ];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
 
-
-    const ALLOWED_UPDATES = ["age","skills","photoUrl","about","gender","userId"];
-    const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
-
-    if(!isUpdateAllowed){
-      throw new Error("Update Not Allowed")
+    if (!isUpdateAllowed) {
+      throw new Error("Update Not Allowed");
     }
 
     if (data.skills.length > 10) {
-      throw new Error("Skills limit cannot exceed 10")
+      throw new Error("Skills limit cannot exceed 10");
     }
 
-
-
-   const user = await User.findByIdAndUpdate({_id : userId},data,{
-    returnDocument:"after",
-    runValidators:true,
-   });
-    res.send("User Updated Successfully")
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User Updated Successfully");
   } catch (error) {
-     res.status(404).send(error.message);
+    res.status(404).send(error.message);
   }
-})
-
+});
 
 connectDB()
   .then(() => {
